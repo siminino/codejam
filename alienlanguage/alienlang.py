3a5abc42f47fa4b741c71e5db7f5048d0985272b
siminino <victor.oliveira.ti@gmail.com>
2011-05-04 00:03:38 -0300

    Refatorando o codigo para funcoes separadas

--- alienlanguage/alienlang.py
+++ alienlanguage/alienlang.py
@@ -5,32 +5,10 @@ def alien_trans(L,D,N,words,text):
     result = []
 
     for test_text in text:
-        letter_possible = []
         caso +=1
-        cont = 0
-        em_paren = False
-        for letter in test_text:
-            letter_possible.append('')
-            if letter == '(':
-                em_paren = True
-            if letter == ')':
-                em_paren = False
 
-            if em_paren:
-                letter_possible[-1] +=letter
-
-        for test_words in words:
-            if test_text == test_words:
-                cont += 1
-            else:
-                for letter_index in range(L):
-                    if (test_words[letter_index] in letter_possible[letter_index]) or (test_words[letter_index] == test_text[letter_index]):
-                        is_equal = True
-                    else:
-                        is_equal = False
-                        break
-                if is_equal:
-                    cont +=1
+        letter_possible = list_letter(test_text)
+        cont = times_in_words(words, test_text, letter_possible, L)
 
         result.append('Case #%d: %s' % (caso, cont))
 
@@ -39,6 +17,36 @@ def alien_trans(L,D,N,words,text):
     return response
 
 
+def list_letter(test_text):
+    em_paren = False
+    letter_possible = []
+    for letter in test_text:
+        if letter == '(':
+            em_paren = True
+            letter_possible.append('')
+        elif letter == ')':
+            em_paren = False
+        elif em_paren == False:
+            letter_possible.append(letter)
+        elif em_paren:
+            letter_possible[-1] +=letter
 
+    return letter_possible
 
+def times_in_words(words, test_text, letter_possible, L):
+    cont = 0
+    for test_words in words:
+        if test_text == test_words:
+            cont += 1
+        else:
+            for letter_index in range(L):
+                if (test_words[letter_index] in letter_possible[letter_index]) or (test_words[letter_index] == test_text[letter_index]):
+                    is_equal = True
+                else:
+                    is_equal = False
+                    break
+            if is_equal:
+                cont +=1
+
+    return cont
 
--- alienlanguage/test.py
+++ alienlanguage/test.py
@@ -1,5 +1,5 @@
 import unittest
-from alienlang import alien_trans
+from alienlang import alien_trans, list_letter
 
 class TestAlienLanguage(unittest.TestCase):
 
@@ -8,5 +8,16 @@ class TestAlienLanguage(unittest.TestCase):
 
     def test_code_jam_2(self):
         self.assertEqual('Case #1: 2\nCase #2: 1\nCase #3: 3\nCase #4: 0', alien_trans(3,5,4,'abc\nbca\ndac\ndbc\ncba','(ab)(bc)(ca)\nabc\n(abc)(abc)(abc)\n(zyx)bc'))
+
+    def test_code_jam_3(self):
+        self.assertEqual('Case #1: 1\nCase #2: 2\nCase #3: 0', alien_trans(3,4,3,'abc\nbca\ndac\ndbc\ncba','(ab)(ca)(abcd)\n(cd)b(ac)\nc(acd)(ac)'))
+
+class TestListLetter(unittest.TestCase):
+
+    def test_deve_retornar_ac_b_dc(self):
+        self.assertEqual(['ac','b','dc'], list_letter('(ac)b(dc)') )
+
+    def test_deve_retornar_acsd_fdseasdds_abc_d_adsaa_asd_poqwea_asdd_a(self):
+        self.assertEqual(['acsd','fdseasdds','abc','d','adsaa','asd','poqwea','asdd','a'], list_letter('(acsd)(fdseasdds)(abc)d(adsaa)(asd)(poqwea)(asdd)a'))
 
 unittest.main()
