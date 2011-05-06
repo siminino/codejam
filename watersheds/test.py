ceaa6df1f4665b7158f0001fac9314725704b671
siminino <victor.oliveira.ti@gmail.com>
2011-05-06 01:13:48 -0300

    Implamentation of function that should return smaller sheed on map

--- watersheds/test.py
+++ watersheds/test.py
@@ -1,5 +1,5 @@
 import unittest
-from watersheds import create_matriz
+from watersheds import create_matriz, min_west_sheed
 
 class TestCreateMatriz(unittest.TestCase):
 
@@ -23,8 +23,6 @@ class TestCreateMatriz(unittest.TestCase):
         self.assertEqual('2 3', len_matriz(3, matriz))
         self.assertEqual([['1','2','3'],['1','2','3']], matriz)
 
-
-
 def len_matriz(W, matriz):
     result = []
     result.append(str(len(matriz)))
@@ -36,5 +34,22 @@ def len_matriz(W, matriz):
     result.append(line_elements)
 
     return ' '.join(result)
+
+class TestMinWestShedd(unittest.TestCase):
+
+    def test_matriz_1_1_should_return_position_0_0(self):
+        self.assertEqual([0,0], min_west_sheed(1, 1, create_matriz(1, 1, '1')))
+
+    def test_matriz_3_3_should_return_position_2_0(self):
+        self.assertEqual([2,0], min_west_sheed(3, 3, create_matriz(3, 3, '9 6 3\n5 9 6\n3 5 9')))
+
+    def test_matriz_2_3_should_return_position_0_1(self):
+        self.assertEqual([0,1], min_west_sheed(2, 3, create_matriz(2, 3, '7 6 7\n7 6 7')))
+
+    def test_matriz_1_10_should_return_position_0_0(self):
+        self.assertEqual([0,0], min_west_sheed(1, 10, create_matriz(1, 10, '0 1 2 3 4 5 6 7 8 7')))
+
+    def test_matriz_8_8_should_return_position_0_0(self):
+        self.assertEqual([0,0], min_west_sheed(2, 13, create_matriz(2, 13, '8 8 8 8 8 8 8 8 8 8 8 8 8\n8 8 8 8 8 8 8 8 8 8 8 8 8')))
 
 unittest.main()
--- watersheds/watersheds.py
+++ watersheds/watersheds.py
@@ -7,3 +7,14 @@ def create_matriz(H, W, content):
 
     return matriz
 
+def min_west_sheed(H,W,matriz):
+    menor = 9999999
+    for item_index in range(W):
+        for line_index in range(H):
+            if int(matriz[line_index][item_index]) < menor:
+                menor = int(matriz[line_index][item_index])
+                line = line_index
+                item = item_index
+
+    return [line, item]
+
