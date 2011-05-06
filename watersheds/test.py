2306830b91ef0b56c0f4d22fbb4bdc4bf062e5f3
siminino <victor.oliveira.ti@gmail.com>
2011-05-06 00:25:57 -0300

    refatorando testes de create_matriz

--- watersheds/test.py
+++ watersheds/test.py
@@ -4,22 +4,29 @@ from watersheds import create_matriz
 class TestCreateMatriz(unittest.TestCase):
 
     def test_1_1_should_return_1(self):
-        self.assertEqual('1 1', len_matriz(1,1,'1'))
-        self.assertEqual([['1']], create_matriz(1,1,'1'))
+        matriz = create_matriz(1,1,'1')
+        self.assertEqual('1 1', len_matriz(1, matriz))
+        self.assertEqual([['1']], matriz)
 
     def test_3_3_should_return_3_3(self):
-        self.assertEqual('3 3', len_matriz(3,3, '1 2 3\n4 5 6\n7 8 9'))
+        matriz = create_matriz(3,3, '1 2 3\n4 5 6\n7 8 9')
+        self.assertEqual('3 3', len_matriz(3, matriz))
+        self.assertEqual([['1','2','3'],['4','5','6'],['7','8','9']], matriz)
 
     def test_5_5_should_return_5_5(self):
-        self.assertEqual('5 5', len_matriz(5,5, '1 2 3 4 5\n1 2 3 4 5\n1 2 3 4 5\n1 2 3 4 5\n1 2 3 4 5'))
+        matriz = create_matriz(5,5, '1 2 3 4 5\n1 2 3 4 5\n1 2 3 4 5\n1 2 3 4 5\n1 2 3 4 5')
+        self.assertEqual('5 5', len_matriz(5, matriz))
+        self.assertEqual([['1','2','3','4','5'],['1','2','3','4','5'],['1','2','3','4','5'],['1','2','3','4','5'],['1','2','3','4','5']], matriz) 
 
     def test_2_3_should_return_2_3(self):
-        self.assertEqual('2 3', len_matriz(2,3, '1 2 3\n1 2 3'))
+        matriz = create_matriz(2,3,'1 2 3\n1 2 3')
+        self.assertEqual('2 3', len_matriz(3, matriz))
+        self.assertEqual([['1','2','3'],['1','2','3']], matriz)
 
 
-def len_matriz(H, W, content):
+
+def len_matriz(W, matriz):
     result = []
-    matriz = create_matriz(H, W, content)
     result.append(str(len(matriz)))
     for item in range(len(matriz)):
         if len(matriz[item]) == W:
